51aa0eb3963856067917a22a5bb1f72916e9d248
kimilm <gnlfla7416@gmail.com>
2022-08-27 17:10:01 +0900

    init project

---
+++ src/main/java/be/podor/PodorApplication.java
@@ -0,0 +1,13 @@
+package be.podor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodorApplication.class, args);
+    }
+
+}
