26c48a5279a96b02bb91664874d5339db62ad6c9
kimilm <gnlfla7416@gmail.com>
2022-09-01 15:16:22 +0900

    feat: 극장 엔티티에 편의시설 연관관계 추가

---
+++ src/main/java/be/podor/theater/dto/ConvenienceResponseDto.java
@@ -0,0 +1,19 @@
+package be.podor.theater.dto;
+
+import be.podor.theater.model.ConvenienceCode;
+import be.podor.theater.model.ConvenienceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConvenienceResponseDto {
+    private String value;
+    private ConvenienceType type;
+
+    public static ConvenienceResponseDto of(ConvenienceCode code) {
+        return new ConvenienceResponseDto(code.getValue(), code.getType());
+    }
+}
---
+++ src/main/java/be/podor/theater/repository/TheaterConvenienceRepository.java
@@ -0,0 +1,8 @@
+package be.podor.theater.repository;
+
+import be.podor.theater.model.TheaterConvenience;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheaterConvenienceRepository extends JpaRepository<TheaterConvenience, Long> {
+
+}
