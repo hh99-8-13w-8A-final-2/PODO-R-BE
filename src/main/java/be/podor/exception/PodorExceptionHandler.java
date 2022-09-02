8dbe2c381756e6e7031c67e88c5fd5123881d624
kimilm <gnlfla7416@gmail.com>
2022-09-02 21:51:34 +0900

    refactor: 슬랙 웹훅 로그백 방식으로 변경

--- src/main/java/be/podor/exception/PodorExceptionHandler.java
+++ src/main/java/be/podor/exception/PodorExceptionHandler.java
@@ -1,24 +1,20 @@
 package be.podor.exception;
 
-import be.podor.slack.SlackWebhook;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class PodorExceptionHandler {
 
-    private final SlackWebhook slackWebhook;
-
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException exception) {
         String errorMessage = exception.getMessage();
-
-        // 슬랙 웹훅 전달
-        slackWebhook.postSlackMessage("[server error] - " + errorMessage);
-
+        log.warn(errorMessage, exception.getCause());
         return ResponseEntity.badRequest().body(errorMessage);
     }
 }
