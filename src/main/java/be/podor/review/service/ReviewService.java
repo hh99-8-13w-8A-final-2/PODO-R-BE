c8156a268b988fc9d01bddab5a67892fc6ebfd5b
kimilm <gnlfla7416@gmail.com>
2022-09-04 00:21:37 +0900

    refactor: 뮤지컬 존재 여부 validator로 감싸기

--- src/main/java/be/podor/review/service/ReviewService.java
+++ src/main/java/be/podor/review/service/ReviewService.java
@@ -2,6 +2,7 @@ package be.podor.review.service;
 
 import be.podor.musical.model.Musical;
 import be.podor.musical.repository.MusicalRepository;
+import be.podor.musical.validator.MusicalValidator;
 import be.podor.review.dto.ReviewLiveResponseDto;
 import be.podor.review.dto.ReviewRequestDto;
 import be.podor.review.model.Review;
@@ -29,9 +30,7 @@ public class ReviewService {
     // 리뷰 작성
     @Transactional
     public Review createReview(Long musicalId, ReviewRequestDto requestDto) {
-        Musical musical = musicalRepository.findById(musicalId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 뮤지컬입니다.")
-        );
+        Musical musical = MusicalValidator.validate(musicalRepository, musicalId);
 
         TheaterSeat theaterSeat = TheaterSeatValidator.validate(theaterSeatRepository, requestDto, musical);
 
