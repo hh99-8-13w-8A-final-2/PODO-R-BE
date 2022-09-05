4d4866bcb0a7a210f9dfed04a6f1356fecf909d4
kimilm <gnlfla7416@gmail.com>
2022-09-05 13:55:46 +0900

    feat: 뮤지컬 리스트 불러오기

---
+++ src/main/java/be/podor/musical/dto/MusicalListResponseDto.java
@@ -0,0 +1,35 @@
+package be.podor.musical.dto;
+
+import be.podor.musical.model.Musical;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MusicalListResponseDto {
+    private Long musicalId;
+    private String musicalName;
+    private String musicalRegion;
+    private String musicalTheater;
+    private String musicalPoster;
+    private LocalDate openDate;
+    private LocalDate closeDate;
+
+    public static MusicalListResponseDto of(Musical musical) {
+        return MusicalListResponseDto.builder()
+                .musicalId(musical.getMusicalId())
+                .musicalName(musical.getMusicalName())
+                .musicalRegion(musical.getTheater().getTheaterAddr())
+                .musicalTheater(musical.getTheater().getTheaterName())
+                .musicalPoster(musical.getMusicalPoster())
+                .openDate(musical.getOpenDate())
+                .closeDate(musical.getCloseDate())
+                .build();
+    }
+}
