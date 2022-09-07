9092f4e5035358fa98f9834d6b461ee3fa2d6692
kimilm <gnlfla7416@gmail.com>
2022-09-07 22:28:38 +0900

    refactor: 변수명 오타 수정

--- src/main/java/be/podor/review/dto/ReviewRequestDto.java
+++ src/main/java/be/podor/review/dto/ReviewRequestDto.java
@@ -13,7 +13,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewRequestDto {
-    private Long theaterId;
     private GradeType grade;
     private FloorType floor;
     private String section;
