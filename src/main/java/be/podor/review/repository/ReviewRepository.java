3b336cde078914874557fe73192e112d8755400f
kimilm <gnlfla7416@gmail.com>
2022-09-09 17:21:44 +0900

    refactor: 라이브 리뷰 엔티티 그래프 설정 / 쿼리 1회만 발생하도록

--- src/main/java/be/podor/review/repository/ReviewRepository.java
+++ src/main/java/be/podor/review/repository/ReviewRepository.java
@@ -6,8 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @EntityGraph(attributePaths = {"theaterSeat"}, type = EntityGraph.EntityGraphType.LOAD)
     Page<Review> findByMusical_MusicalIdOrderByCreatedAtDesc(Long musicalId, Pageable pageable);
+
+    // 라이브 리뷰
+    @EntityGraph(attributePaths = {"musical", "theaterSeat"}, type = EntityGraph.EntityGraphType.LOAD)
+    List<Review> findTop10ByOrderByCreatedAtDesc();
 }
