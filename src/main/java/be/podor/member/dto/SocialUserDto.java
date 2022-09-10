73cf23d460604cb8ebf1632d439538fd8d3710b2
kimilm <gnlfla7416@gmail.com>
2022-09-10 15:10:28 +0900

    refactor: 헤더 리턴 dto에서 수행하도록 변경

--- src/main/java/be/podor/member/dto/SocialUserDto.java
+++ src/main/java/be/podor/member/dto/SocialUserDto.java
@@ -1,10 +1,12 @@
 package be.podor.member.dto;
 
 
+import be.podor.security.jwt.JwtFilter;
 import be.podor.security.jwt.TokenDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpHeaders;
 
 
 @Getter
@@ -14,4 +16,11 @@ public class SocialUserDto {
     private MemberDto memberDto;
     private TokenDto tokenDto;
 
+    public HttpHeaders getTokenHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JwtFilter.AUTHORIZATION_HEADER, tokenDto.getAccessToken());
+        headers.add(JwtFilter.REFRESH_TOKEN_HEADER, tokenDto.getRefreshToken());
+
+        return headers;
+    }
 }
