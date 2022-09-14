7ec98559648b1aebb800d172a4b3a536496c6d8c
hunkim00 <kimhun1027@naver.com>
2022-09-14 20:00:22 +0900

    feat : 닉네임 수정 유효성 추가

--- src/main/java/be/podor/mypage/dto/MyPageRequestDto.java
+++ src/main/java/be/podor/mypage/dto/MyPageRequestDto.java
@@ -4,9 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class MyPageRequestDto {
+    @NotBlank(message = "닉네임은 공백일 수 없습니다.")
+    @Size(min = 2, max = 20)
+    @Pattern(regexp = "[ㄱ-ㅎ가-힣a-zA-Z\\d]{1,20}$")
     private String nickname;
 }
