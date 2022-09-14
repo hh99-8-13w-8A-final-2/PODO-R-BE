6e90be2ab54a6f5759311c0efb77ac7345754cda
kimilm <gnlfla7416@gmail.com>
2022-09-14 18:16:39 +0900

    feat: 이미지 리사이징 구현

--- src/main/java/be/podor/aws/service/S3Service.java
+++ src/main/java/be/podor/aws/service/S3Service.java
@@ -5,11 +5,17 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +23,8 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class S3Service {
+
+    private static final int IMAGE_WIDTH = 800;
 
     private final AmazonS3 amazonS3;
 
@@ -32,13 +40,16 @@ public class S3Service {
 
             String fileName = UUID.randomUUID().toString();
 
+            BufferedImage image = resizeImage(multipartFile, IMAGE_WIDTH);
+            String contentType = multipartFile.getContentType();
             ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentType(multipartFile.getContentType());
+
+            ByteArrayInputStream inputStream = imageToInputStream(image, contentType, objectMetadata);
 
             PutObjectRequest putObjectRequest = new PutObjectRequest(
                     bucket,
                     fileName,
-                    multipartFile.getInputStream(),
+                    inputStream,
                     objectMetadata
             );
 
@@ -49,5 +60,29 @@ public class S3Service {
 
 
         return new S3Dto(imgurls);
+    }
+
+    // image resize
+    BufferedImage resizeImage(MultipartFile multipartFile, int targetWidth) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(multipartFile.getInputStream());
+
+        if (bufferedImage.getWidth() < targetWidth) {
+            return bufferedImage;
+        }
+
+        return Scalr.resize(bufferedImage, targetWidth);
+    }
+
+    // image to InputStream
+    ByteArrayInputStream imageToInputStream(BufferedImage bufferedImage, String contentType, ObjectMetadata objectMetadata) throws IOException {
+        String fileExtension = contentType.split("/")[1];
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, fileExtension, byteArrayOutputStream);
+
+        objectMetadata.setContentType(contentType);
+        objectMetadata.setContentLength(byteArrayOutputStream.size());
+
+        return new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
     }
 }
