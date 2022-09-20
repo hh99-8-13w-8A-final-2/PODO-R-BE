2640d35241eee709ff0eb6173c39aef43ebc3800
kimilm <gnlfla7416@gmail.com>
2022-09-20 21:44:03 +0900

    feat: 이미지 삭제 기능 구현

--- src/main/java/be/podor/aws/controller/S3Controller.java
+++ src/main/java/be/podor/aws/controller/S3Controller.java
@@ -1,12 +1,11 @@
 package be.podor.aws.controller;
 
+import be.podor.aws.dto.ImageUrlDto;
 import be.podor.aws.dto.S3Dto;
 import be.podor.aws.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -19,11 +18,18 @@ public class S3Controller {
     private final S3Service s3Service;
 
     // 이미지 업로드
-    // Todo member permit
     @PostMapping("/api/image/upload")
     public ResponseEntity<?> uploadImage(@RequestPart("image") List<MultipartFile> multipartFiles) throws IOException {
         S3Dto responseDto = s3Service.s3FileUpload(multipartFiles);
 
         return ResponseEntity.ok(responseDto);
+    }
+
+    // 이미지 삭제
+    @DeleteMapping("/api/image/delete")
+    public ResponseEntity<?> deleteImage(@RequestBody ImageUrlDto requestBody) {
+        s3Service.s3FileDelete(requestBody);
+
+        return ResponseEntity.ok().build();
     }
 }
