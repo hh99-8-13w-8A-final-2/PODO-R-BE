be15ac30115dd5d4eebff1ec1fb1c0e61606d118
kimilm <gnlfla7416@gmail.com>
2022-09-24 19:50:52 +0900

    feat: 뮤지컬별 인기 태그 조회 구현

--- src/main/java/be/podor/tag/controller/TagController.java
+++ src/main/java/be/podor/tag/controller/TagController.java
@@ -3,6 +3,8 @@ package be.podor.tag.controller;
 import be.podor.tag.dto.TagsResponseDto;
 import be.podor.tag.service.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +19,16 @@ public class TagController {
     @GetMapping("/api/tags")
     public ResponseEntity<?> getTagsStartWith(@RequestParam(name = "tag", defaultValue = "") String tag) {
         TagsResponseDto responseDto = tagService.getTagsStartWith(tag);
+
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/api/tags/popular")
+    public ResponseEntity<?> getPopularTags(
+            @RequestParam(name = "musicalId") Long musicalId,
+            @PageableDefault(size = 15) Pageable pageable
+    ) {
+        TagsResponseDto responseDto = tagService.getPopularTags(musicalId, pageable);
 
         return ResponseEntity.ok(responseDto);
     }
--- src/main/java/be/podor/tag/service/TagService.java
+++ src/main/java/be/podor/tag/service/TagService.java
@@ -1,9 +1,12 @@
 package be.podor.tag.service;
 
+import be.podor.review.repository.ReviewTagRepository;
 import be.podor.tag.dto.TagsResponseDto;
 import be.podor.tag.model.Tag;
 import be.podor.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,11 +18,20 @@ public class TagService {
 
     private final TagRepository tagRepository;
 
+    private final ReviewTagRepository reviewTagRepository;
+
     public TagsResponseDto getTagsStartWith(String tag) {
         List<Tag> tags = tagRepository.findTop10ByTagStartsWith(tag);
 
         return new TagsResponseDto(tags.stream()
                 .map(Tag::getTag)
                 .collect(Collectors.toList()));
+    }
+
+    @Cacheable(value = "popularTags", key = "#musicalId")
+    public TagsResponseDto getPopularTags(Long musicalId, Pageable pageable) {
+        List<String> tags = reviewTagRepository.findPopularTags(musicalId, pageable);
+
+        return new TagsResponseDto(tags);
     }
 }
