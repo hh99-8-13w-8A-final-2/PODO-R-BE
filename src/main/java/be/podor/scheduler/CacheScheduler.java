bd883c63a09a5e725d498e67505fb2757d79eeb6
kimilm <gnlfla7416@gmail.com>
2022-09-24 18:28:40 +0900

    feat: 스케줄러 설정

---
+++ src/main/java/be/podor/scheduler/CacheScheduler.java
@@ -0,0 +1,9 @@
+package be.podor.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CacheScheduler {
+}
