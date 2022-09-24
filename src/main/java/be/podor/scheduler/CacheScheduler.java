f95801ed2212c78d406a6733793cc1548f5b6926
kimilm <gnlfla7416@gmail.com>
2022-09-24 20:37:25 +0900

    feat: 뮤지컬별 인기 태그 캐시 만료 설정

--- src/main/java/be/podor/scheduler/CacheScheduler.java
+++ src/main/java/be/podor/scheduler/CacheScheduler.java
@@ -1,9 +1,29 @@
 package be.podor.scheduler;
 
+import be.podor.musical.service.MusicalService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CacheScheduler {
+
+    private final MusicalService musicalService;
+
+    // 10분마다
+    @Scheduled(cron = "0 */10 * * * *")
+    public void refreshPopularTags() {
+        log.info("open musical popular tag cache refresh");
+
+        musicalService.getOpenMusical().forEach(musical -> evictPopularTagCache(musical.getMusicalId()));
+    }
+
+    @CacheEvict(value = "popularTags", key = "#musicalId")
+    public void evictPopularTagCache(Long musicalId) {
+
+    }
 }
