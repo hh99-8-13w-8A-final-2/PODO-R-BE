e9cbd42bc178a55d0e8fc0624e58acbef8ef6fc5
kimilm <gnlfla7416@gmail.com>
2022-09-26 15:01:47 +0900

    feat: 최근 검색어 엔티티 생성

---
+++ src/main/java/be/podor/member/model/MemberSearch.java
@@ -0,0 +1,21 @@
+package be.podor.member.model;
+
+import be.podor.share.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberSearch extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberSearchId;
+
+    @Column(nullable = false)
+    private String search;
+}
