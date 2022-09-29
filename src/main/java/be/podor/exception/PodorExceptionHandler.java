01bee7e152b7657af5d40c3fb2ab224e65aa4e13
kimilm <gnlfla7416@gmail.com>
2022-09-30 04:47:36 +0900

    refactor: @Valid 예외 메시지 수정

--- src/main/java/be/podor/exception/PodorExceptionHandler.java
+++ src/main/java/be/podor/exception/PodorExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -33,8 +35,20 @@ public class PodorExceptionHandler {
     // @Valid 예외
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        log.warn(exception.getMessage(), exception.getCause());
-        return ResponseEntity.badRequest().body(exception.getMessage());
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]\n");
+        }
+        log.warn(builder.toString());
+        return ResponseEntity.badRequest().body(builder.toString());
     }
 
     // Podoal 통합 예외
