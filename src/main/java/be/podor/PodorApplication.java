fb29ecfe0f66dc071480e1557ec6e14442d455e6
kimilm <gnlfla7416@gmail.com>
2022-10-06 23:23:21 +0900

    refactor: auditing config 파일 분리

--- src/main/java/be/podor/PodorApplication.java
+++ src/main/java/be/podor/PodorApplication.java
@@ -2,12 +2,8 @@ package be.podor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class PodorApplication {
 
     static {
